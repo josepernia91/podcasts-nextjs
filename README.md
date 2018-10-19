099bccb767e0a41bb0a8cf96b0ff31cc3129e440
Jose Pernia <josepernia91@gmail.com>
2018-10-19 18:46:39 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+NextJS test, hitting Audioboom API
---
+++ pages/index.js
@@ -0,0 +1,66 @@
+import 'isomorphic-fetch';
+
+export default class extends React.Component {
+
+  static async getInitialProps() {
+    let req = await fetch('https://api.audioboom.com/channels/recommended');
+    let { body: channels } = await req.json();
+
+    return { channels };
+  }
+
+  render() {
+    const { channels  } = this.props;
+    return(
+      <div>
+        <header>Podcasts</header>
+
+        <div className="channels">
+          { channels.map((channel) => (
+            <div className="channel">
+              <img src={ channel.urls.logo_image.original } alt="Channel Logo" />
+              <h2>{ channel.title }</h2>
+            </div>
+          )) }
+        </div>
+
+        <style jsx>{`
+          header {
+            color: #fff;
+            background: #8756ca;
+            padding: 15px;
+            text-align: center;
+          }
+          .channels {
+            display: grid;
+            grid-gap: 15px;
+            padding: 15px;
+            grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+          }
+          .channel {
+            display: block;
+            box-shadow: 0 2px 6px rgba(0,0,0,0.15);
+            margin-bottom: 0.5em;
+          }
+          .channel img {
+            width: 100%;
+          }
+          .channel h2 {
+            padding: 5px;
+            font-size: 0.9em;
+            font-weight: 600;
+            margin: 0;
+            text-align: center;
+          }
+      `}</style>
+      <style jsx global>{`
+          body {
+            margin: 0;
+            font-family: system-ui;
+            background_ white;
+          }
+      `}</style>
+      </div>
+    );
+  }
+}
